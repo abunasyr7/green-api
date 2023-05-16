3105addd89edd79c74d6c9c1e1086e71be05064b
Abunasyr <a.syrmanov@shinhanfinance.kz>
2023-05-16 12:28:19 +0600

    feat: add contacts and chats

--- src/components/Contacts.jsx
+++ src/components/Contacts.jsx
@@ -1,11 +1,27 @@
 import "./Contacts.css";
+import MessangerIcon from "../img/messenger.png"
 
 const Contacts = () => {
+
+
   return (
     <div className="contacts">
+      <img src={MessangerIcon} alt="Messanger Icon" style={{width: 25, height: 25}} />
       <ul>
-        <li>Contact 1</li>
-        <li>Contact 2</li>
+        <li className="contacts__container">
+          <div className="photo"></div>
+          <div className="contacts__info">
+            <h1>Name</h1>
+            <p>Message</p>
+          </div>
+        </li>
+        <li className="contacts__container">
+          <div className="photo"></div>
+          <div className="contacts__info">
+            <h1>Name</h1>
+            <p>Message</p>
+          </div>
+        </li>
       </ul>
     </div>
   );
